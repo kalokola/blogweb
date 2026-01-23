c035dd1e11cbd481d7d246da020c202dcb940c00
kalokola <brightiuskalokola@gmail.com>
2026-01-23 20:59:18 +0300

    models-views-urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home-page'),
-    path('about/', views.about_page, name='about-page'),
+    path('', views.home_page, name = 'home-page'),
+    path('about/', views.about_page, name = 'about-page'),
     path('posts/', views.all_posts, name = 'all-posts')
 ]
